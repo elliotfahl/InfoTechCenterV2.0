62a941f023b5a6ebc23c4db45e4e6edb96f11e9f
Fahl <2elliot.fahl@gulllakecs.org>
2024-02-12 12:57:27 -0500

    Added library time - called time stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,3 +1,8 @@
-print("\n\nWelcome at InfoTech Center V1.0")
+#Import Libraries Here
+import time
 
+timeToSleep = 1.5
+
+print("\n\nWelcome at InfoTech Center V1.0")
+time.sleep(timeToSleep)
 print("\nInfo Tech Center System Booting...")
