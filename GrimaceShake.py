2d94a46af04ddb2b2bb7b3ac21462864192ed50c
Fahl <2elliot.fahl@gulllakecs.org>
2024-02-12 14:16:49 -0500

    Add Ellipsis While Loop - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,8 +1,23 @@
 #Import Libraries Here
 import time
+import sys
 
 timeToSleep = 1.5
 
 print("\n\nWelcome at InfoTech Center V1.0")
 time.sleep(timeToSleep)
-print("\nInfo Tech Center System Booting...")
+
+#Code to have the ellipsis add a dot every .5 seconds
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("Info Tech Center System Booting" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) # \r prints a carriage return first
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
