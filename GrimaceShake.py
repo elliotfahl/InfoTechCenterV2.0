bce12419096100373d7b16e76276822d6ec55dae
Fahl <2elliot.fahl@gulllakecs.org>
2024-02-26 13:25:08 -0500

    Added ellipsis

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -27,7 +27,7 @@ def gasLevelAlert():
         sleep(2.5)
         print("    ***Calling Triple AAA***")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is extremely low, checking Google Maps for the closest gas station")
+        print("Your gas tank is extremely low, checking Google Maps for the closest gas station...")
         sleep(2.5)
         print("The closest gas station is",listOfGasStations(),"which is",milesToGasStationsLow,"miles away.")
     elif gasLevelIndicator == "Quarter Tank":
